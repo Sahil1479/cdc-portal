f58d5628bcee97c7bd63d16fb53d55afe006a0bb
Sahil Harpal <56965873+Sahil1479@users.noreply.github.com>
2021-01-05 00:25:45 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App">
+    <div className="App">
       <Router>
         <div>
           <Switch>
